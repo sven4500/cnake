489483dffcd3384325b2a6e642afcb5700f4ab25
sven4500 <sven4500@mail.ru>
2021-03-16 02:00:48 +0300

    Игнорируется дополнительный код клавиш стрелок. Нельзя менять направление на противоположное. Генерируется еда в случайной точке.

--- xnake.cpp
+++ xnake.cpp
@@ -2,8 +2,9 @@
 #include <vector>
 #include <thread> // sleep_for
 #include <chrono> // seconds
+#include <conio.h> // getch
 
-enum direction { left, right, up, down };
+enum direction { left = 75, right = 77, up = 72, down = 80 };
 
 struct point
 {
@@ -33,12 +34,63 @@ int main()
 {
 	std::vector<point> snake{ point(39, 10), point(40, 10), point(41, 10) };
 	point head = snake.front();
+	point food = point(0, 0);
 	direction dir = left;
 
-	int const width = 119, height = 29;
+	int const width = 59, height = 29;
 
 	while (1)
 	{
+		if (kbhit())
+		{
+			int const key = getch();
+
+			// Для стрелок возвращается два кода. Сперва код откуда была нажата стрелка
+			// 224 для непосредственно стрелок и 0 для numpad'а. Поток код стрелки. Эти
+			// коды не являются универсальными и могут отличаться на других платформах.
+			if (key == 224 || key == 0)
+			{
+				continue;
+			}
+
+			// Запрещаем противоположные направления.
+			if ((key == left && dir != right) ||
+				(key == right && dir != left) ||
+				(key == up && dir != down) ||
+				(key == down && dir != up))
+			{
+				dir = (direction)key;
+			}
+		}
+
+		if (dir == left)
+		{
+			head.x -= 1;
+		}
+		else if (dir == right)
+		{
+			head.x += 1;
+		}
+		else if (dir == up)
+		{
+			head.y -= 1;
+		}
+		else if (dir == down)
+		{
+			head.y += 1;
+		}
+
+		if (food == point(0, 0))
+		{
+			food = point(1 + rand() % (width - 2), 1 + rand() % (height - 2));
+		}
+
+		snake.pop_back();
+		snake.insert(snake.begin(), head);
+		
+		// Не кросс-платформенное решение, но лучше пока не наёдено.
+		std::system("cls");
+
 		for (int y = 0; y < height; ++y)
 		{
 			for (int x = 0; x < width; ++x)
@@ -54,28 +106,56 @@ int main()
 						std::cout << '+';
 					}
 				}
-				else
-					if (x == 0 || x == width - 1)
+				else if (x == 0 || x == width - 1)
+				{
+					if (y > 0 && y < height)
 					{
-						if (y > 0 && y < height)
-						{
-							std::cout << '|';
-						}
+						std::cout << '|';
+					}
+				}
+				else if (x == food.x&&y == food.y)
+				{
+					std::cout << 'o';
+				}
+				else if (x == head.x && y == head.y)
+				{
+					if (dir == left)
+					{
+						std::cout << '<';
+					}
+					else if (dir == right)
+					{
+						std::cout << '>';
+					}
+					else if (dir == up)
+					{
+						std::cout << 'A';
+					}
+					else if (dir == down)
+					{
+						std::cout << 'V';
+					}
+				}
+				else if (std::find(snake.begin(), snake.end(), point(x, y)) != snake.end())
+				{
+					if (snake.back() == point(x, y))
+					{
+						std::cout << '?';
 					}
 					else
-						if (std::find(snake.begin(), snake.end(), point(x, y)) != snake.end())
-						{
-							std::cout << 'o';
-						}
-						else
-						{
-							std::cout << ' ';
-						}
+					{
+						std::cout << '.';
+					}
+				}
+				else
+				{
+					std::cout << ' ';
+				}
 			}
 
 			std::cout << std::endl;
 		}
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 }
